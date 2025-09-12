f3085244b468da0a3af1695b5bb611fa300bc2ab
AANIYA JAISWAL <143058360+aaniyajais@users.noreply.github.com>
2025-09-12 19:40:49 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,12 +5,14 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",, // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
-  });
 
-  return token;
-};
+
+res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+  secure: process.env.NODE_ENV !== "development",
+  path: "/",
+});
+ return token;
+  };
