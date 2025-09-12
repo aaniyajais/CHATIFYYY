051c2a62f0c4a6f7a434be174acf1bc6c6e89bfb
AANIYA JAISWAL <143058360+aaniyajais@users.noreply.github.com>
2025-09-12 19:56:57 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatifyyy-backend.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://chatifyyy-backend.onrender.com/api",
   withCredentials: true,
 });
+
 
 // Add response interceptor to handle errors
 axiosInstance.interceptors.response.use(
