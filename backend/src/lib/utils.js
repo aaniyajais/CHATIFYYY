95bd24888113fff026510259bf8d2e9cc15b31bf
AANIYA JAISWAL <143058360+aaniyajais@users.noreply.github.com>
2025-09-14 22:50:58 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,8 +11,10 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-  secure: process.env.NODE_ENV !== "development",
+//  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+  //secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
+sameSite: "lax",
   path: "/",
 });
  return token;
